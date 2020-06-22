ed056b2f43e74f8d0c13b3db0062d7ea8e6b3aba
kosmik8485 <kosmik8485@gmail.com>
2020-06-22 11:44:47 +0300

    Create ClientProxy.java

---
+++ src/main/ru/msk/homesrv2/proxy/ClientProxy.java
@@ -0,0 +1,21 @@
+public class ClientProxy extends CommonProxy
+{
+    @Override
+    public void preInit(FMLPreInitialization event)
+    {
+        super.preInit(event);
+    }
+
+    @Override
+    public void init(FMLInitialization event)
+    {
+        super.init(event);
+    }
+
+    @Override
+    public void postInit(FMLPostInitialization event)
+    {
+        super.postInit(event);
+    }
+
+}
