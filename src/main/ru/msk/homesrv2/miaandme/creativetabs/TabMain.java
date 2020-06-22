5d79cd66833874f6ec20b65f2c4da4c4136a4fcb
kosmik8485 <kosmik8485@gmail.com>
2020-06-22 13:37:24 +0300

    Create TabMain.java

---
+++ src/main/ru/msk/homesrv2/miaandme/creativetabs/TabMain.java
@@ -0,0 +1,15 @@
+package ru.msk.jomesrv2.miaandme.creativetabs;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+
+public class TabMain extends CreativeTabs {
+	public TabMain(String label) {
+		super(label);
+	}
+	@Override
+	public ItemStack getTabIconItem() {
+		return new ItemStack(Items.APPLE);
+	}
+}
